3a90c793b522ff24b24d19714bba239792c3bcb7
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-02 21:22:19 -0500

    new

---
+++ bot.py
@@ -0,0 +1,11 @@
+from instabot import Bot
+
+
+bot = Bot()
+
+bot.login(username = "aladdins_carpet",
+password = "Sabdeljabbar98")
+
+bot.upload_photo("picture.jpg",
+caption = "another automation")
+
